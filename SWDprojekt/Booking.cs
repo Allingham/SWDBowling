7abf385a1be8df22ded18632d06f5bd50d7c27e5
Christian Allingham <christtianallingham@gmail.com>
2019-11-20 09:54:53 +0100

    Add project files.

---
+++ SWDprojekt/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.PointOfService;
+
+namespace SWDprojekt
+{
+    class Booking
+    {
+        public ObservableCollection<Deltager> Deltagere { get; set; }
+        public DateTime Dato{ get; set; }
+        public Bane ValgtBane{ get; set; }
+
+        public Booking(ObservableCollection<Deltager> delagere, DateTime dato, Bane valgtBane){
+            Deltagere = delagere;
+            Dato = dato;
+            ValgtBane = valgtBane;
+        }
+
+     
+
+        public void Add(String name, bool voksen)
+        {
+            Deltagere.Add(new Deltager(name, voksen));
+        }
+    }
+}
